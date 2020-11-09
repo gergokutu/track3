b9ccef092a2f0736a5b2c0f07a7522a7c57ed25b
Subash Pradahan <suprowilde5@gmail.com>
2020-11-09 15:32:25 +0100

    Adding test to check if classes are created using new() keywords

--- src/__tests__/setNavbar.test.ts
+++ src/__tests__/setNavbar.test.ts
@@ -1,19 +1,6 @@
 import Navbar from "../Navbar/setNavbar";
 
-const mockChangeMenuState = jest.fn();
-jest.mock("../Navbar/setNavbar", () => {
-  return jest.fn().mockImplementation(() => {
-    return {
-      changeMenuState: mockChangeMenuState,
-    };
-  });
-});
-
 describe("Check The Class Methods", () => {
-  beforeEach(() => {
-    mockChangeMenuState.mockClear();
-  });
-
   it("Check If The Class Is Called With The New Keyword", () => {
     const container = document.createElement("div");
     const navbar = new Navbar(container);
